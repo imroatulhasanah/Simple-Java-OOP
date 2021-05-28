2ea1671e90de733286ffdcc07c09266cf69cb183
Robby Alamsyah <47165793+robby0125@users.noreply.github.com>
2021-05-28 20:09:47 +0700

    Create SimpleOOP.java

---
+++ SimpleOOP.java
@@ -0,0 +1,12 @@
+class Car {
+    void startEngine() {
+        System.out.println("Engine started!");
+    }
+}
+
+public class SimpleOOP {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.startEngine();
+    }
+}
